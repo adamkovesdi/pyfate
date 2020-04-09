2fbe49cdae92798aefe53d916518426077f46172
adamkov <adamkov@192.168.2.9>
2020-04-09 09:50:11 +0200

    one line json

--- core.py
+++ core.py
@@ -64,11 +64,18 @@ def create_logentry(rollobjects, message):
 def save_logbook(logbook):
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
     my_file = os.path.join(THIS_FOLDER, 'logbook.json')
-    with open(my_file, 'w') as fp:
-        json.dump(logbook, fp)
+    with open(my_file, 'a+') as fp:
+        for l in logbook:
+            s = json.dumps(l)
+            s = s + "\n"
+            fp.write(s)
 
 def load_logbook():
+    logbook = []
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
     my_file = os.path.join(THIS_FOLDER, 'logbook.json')
     with open(my_file, 'r') as fp:
-        return json.load(fp)
+        lines = fp.readlines()
+        for l in lines:
+            logbook.append(json.loads(l))
+    return logbook
