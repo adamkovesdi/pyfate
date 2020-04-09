ac8044b3794ac8e9a658049168bf492763469974
adamkov <adamkov@192.168.2.9>
2020-04-09 08:32:27 +0200

    replace [+] dice notation with fontawesome icons

---
+++ textreplacer.py
@@ -0,0 +1,46 @@
+# Replace Fate dice symbols in text strings
+
+
+def findfate(s):
+    s = s.replace("\n", '<br />')
+    s = s.replace("[+]", '<i class="fa fa-plus-square-o"></i> ')
+    s = s.replace("[-]", '<i class="fa fa-minus-square-o"></i> ')
+    s = s.replace("[ ]", '<i class="fa fa-square-o"></i> ')
+    spin = '<i class="fa fa-spinner fa-spin"></i> '
+    return spin + s
+
+rawinput='''This is a new roll
+[+][ ][-][-] + 3 = 2
+'''
+
+front = '''
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Fate dice</title>
+  </head>
+  <body>
+  <h2>Fate dice roller</h2>
+'''
+
+back = '''
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+'''
+
+print(front)
+print(findfate(rawinput))
+print(back)
