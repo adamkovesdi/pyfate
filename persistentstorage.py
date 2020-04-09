ac67e59a1ed3213bd9e16e29066e6676699e7c80
adamkov <adamkov@192.168.2.9>
2020-04-09 10:22:02 +0200

    Persistent storage module

---
+++ persistentstorage.py
@@ -0,0 +1,31 @@
+# Persistent storage module for pyfate
+
+import os
+import json
+
+def save_logbook(logbook):
+    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+    my_file = os.path.join(THIS_FOLDER, 'logbook.json')
+    with open(my_file, 'a+') as fp:
+        for l in logbook:
+            s = json.dumps(l)
+            s = s + "\n"
+            fp.write(s)
+
+def appendentry_save(l):
+    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+    my_file = os.path.join(THIS_FOLDER, 'logbook.json')
+    with open(my_file, 'a+') as fp:
+        s = json.dumps(l)
+        s = s + "\n"
+        fp.write(s)
+
+def load_logbook():
+    logbook = []
+    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+    my_file = os.path.join(THIS_FOLDER, 'logbook.json')
+    with open(my_file, 'r') as fp:
+        lines = fp.readlines()
+        for l in lines:
+            logbook.append(json.loads(l))
+    return logbook
